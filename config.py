8ea5db69c207b73540604e101dae23cb91c9040d
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-01-26 12:13:32 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+from os import getenv
+
+class Config(object):
+      API_HASH = getenv("API_HASH", "5264bf4663e9159565603522f58d3c18")
+      API_ID = int(getenv("API_ID", 11973721))
+      BOT_TOKEN = getenv("BOT_TOKEN", "5949999646:AAGNAzsUTMutsqtnSk2R4MkGgCQ0uMtqbIU")
