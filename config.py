02fe74035b527085f4cf4d6a6cbc3789d05ad536
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-01-26 12:43:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,20 @@
 from os import getenv
+import os
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S'
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
 
 class Config(object):
       API_HASH = getenv("API_HASH", "5264bf4663e9159565603522f58d3c18")
       API_ID = int(getenv("API_ID", 11973721))
       BOT_TOKEN = getenv("BOT_TOKEN", "5949999646:AAGNAzsUTMutsqtnSk2R4MkGgCQ0uMtqbIU")
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
